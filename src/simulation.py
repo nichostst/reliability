f3b84d381742c1470c0aca62834c826349af74e6
Nicholas ST <nichostst@pop-os.localdomain>
2020-05-04 06:05:26 +0800

    Added trigger to allow repeated simulation

--- src/simulation.py
+++ src/simulation.py
@@ -14,14 +14,18 @@ def simulation(dist):
     cutoff = st.slider('Maintenance Interval', int(mag*0.2),
                        min([int(mag*10), 1000000]), int(mag*0.8), 100)
     sim = Simulation(dist, cutoff, size)
-    sim.simulate()
-    df = pd.DataFrame({'time': sim.trials})
-    csv = df.to_csv(index=False)
 
-    # Strings <-> bytes conversion
-    b64 = base64.b64encode(csv.encode()).decode()
-    href = '<a href="data:file/csv;base64,{}" download="{}">Download CSV</a>'
-    filename = st.text_input('Filename (e.g. data.csv)', 'data.csv')
+    btn = st.button('Simulate')
+    if btn:
+        sim.simulate()
+        df = pd.DataFrame({'time': sim.trials})
+        csv = df.to_csv(index=False)
 
-    st.markdown(href.format(b64, filename), unsafe_allow_html=True)
-    st.write('Development in progress...')
+        # Strings <-> bytes conversion
+        b64 = base64.b64encode(csv.encode()).decode()
+        href = '''<a href="data:file/csv;base64,{}"
+               download="{}">Download CSV</a>'''
+        filename = st.text_input('Filename (e.g. data.csv)', 'data.csv')
+
+        st.markdown(href.format(b64, filename), unsafe_allow_html=True)
+        st.write('Development in progress...')
